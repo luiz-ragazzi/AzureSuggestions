21ae419d257628ce08d71dde07212723b2cee9a6
Luiz Ragazzi <lr@labee.co.uk>
2015-09-16 15:39:45 +0100

    first commit

---
+++ AzureSuggestion/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using AzureSearchSuggestionsDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureSuggestion.Controllers
+{
+    public class SearchController : Controller
+    {
+        private SuggestionSearch _suggestSearch = new SuggestionSearch();
+
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Suggest(string q, bool fuzzy)
+        {
+            // Call suggest query and return results
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = _suggestSearch.Suggest(q, fuzzy)
+            };
+
+        }
+    }
+}
---
+++ AzureSuggestion/SuggestionSearch.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+
+namespace AzureSearchSuggestionsDemo
+{
+    public class SuggestionSearch
+    {
+        public class Suggestion
+        {
+
+            public string value { get; set; }
+            public string OfficeName { get; set; }
+            public string PersonName { get; set; }
+            public string PersonID { get; set; }
+        }
+
+       
+
+        private readonly Uri _serviceUri;
+        private HttpClient _httpClient;
+
+        public SuggestionSearch()
+        {
+            _serviceUri = new Uri("https://" + ConfigurationManager.AppSettings["SearchServiceName"] + ".search.windows.net");
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("api-key", ConfigurationManager.AppSettings["SearchServiceApiKey"]);
+        }
+
+        public dynamic Suggest(string searchText, bool fuzzy)
+        {
+            // Pass the specified suggestion text and return the fields
+            Uri uri = new Uri(_serviceUri, "/indexes/offices/docs/suggest?suggesterName=sgOffices&$top=10&$select=PersonName,OfficeName, PersonID&fuzzy=" + fuzzy + "&search=" + Uri.EscapeDataString(searchText));
+            HttpResponseMessage response = AzureSearchHelper.SendSearchRequest(_httpClient, HttpMethod.Get, uri);
+            AzureSearchHelper.EnsureSuccessfulSearchResponse(response);
+            List<Suggestion> suggestionList = new List<Suggestion>();
+
+            foreach (var option in AzureSearchHelper.DeserializeJson<dynamic>(response.Content.ReadAsStringAsync().Result).value)
+            {
+                Suggestion suggestion = new Suggestion();
+                //suggestion.id = (string)option["id"];
+                suggestion.value = (string)option["@search.text"];
+                suggestion.OfficeName = (string)option["OfficeName"];
+                suggestion.PersonName = (string)option["PersonName"];
+                suggestion.PersonID = (string)option["PersonID"];
+                //suggestion.thumbnail = (string)option["thumbnail"];
+                suggestionList.Add(suggestion);
+            }
+
+            return suggestionList;
+        }
+    }
+}
+
+
